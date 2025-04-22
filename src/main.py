58d1011df63b9800caf49f3004817adf91f5d5cd
DavidSanchezCertus <david.sanchez@rlbusiness.com.mx>
2025-04-22 09:45:19 -0700

    Listo para EC2

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
-def main():
-    print("¡Hola, mundo!")
+from flask import Flask
 
-if __name__ == "__main__":
-    main()
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "¡Hola, mundo desde AWS EC2!"
+
+if __name__ == '__main__':
+    # 0.0.0.0 permite acceder desde cualquier IP
+    # puerto 8080 es común para aplicaciones web
+    app.run(host='0.0.0.0', port=8080)
